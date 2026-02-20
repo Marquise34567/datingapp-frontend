14710a5d12fc47ce7ea7b0e9e045850c087e0ba3
Auto-Editor User <user@auto-editor.local>
2026-02-20 00:17:10 -0500

    frontend: push changes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import Maintenance from "./components/Maintenance";
+import PremiumDatingAdvicePage from "./components/PremiumDatingAdvicePage";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <PremiumDatingAdvicePage />
     <Maintenance />
   </React.StrictMode>
 );
